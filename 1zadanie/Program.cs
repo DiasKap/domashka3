f9f4c5a8c99b16db07a51af01ac74a1d3dedeb4c
DiasKapassov <diasgoose@gmail.com>
2023-04-01 21:11:49 +0600

    Сделал 1 задание

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");  
+            int number = int.Parse(Console.ReadLine());  
+            int reverse = 0;
+            int number2 = number;
+            while(number2>0)  
+            {  
+                int reminder = number2 % 10;  
+                reverse = (reverse * 10) + reminder;  
+                number2 = number2 / 10;  
+            }  
+            if (number == reverse) {
+                Console.WriteLine("Это палиндром");
+            }
+            else {
+                Console.WriteLine("Не палиндром");
+            }
