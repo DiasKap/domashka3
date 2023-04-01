a541e20f753f820c3cd84aaee4f238038699ddba
DiasKapassov <diasgoose@gmail.com>
2023-04-01 21:00:41 +0600

    Сделал 3 задание

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Число: ");
+int num = int.Parse(Console.ReadLine());
+for (int i=1; i < num + 1; i++)
+{
+    if(i!=num)
+{
+    Console.Write(Math.Pow(i,3)+ " ");
+        }
+    else
+    {
+        
+    Console.Write(Math.Pow(i,3)+ " ");
+    }
+
+}
